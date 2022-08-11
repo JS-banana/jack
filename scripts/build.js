366a083349469b76be9ed64f019127cf4d6f9317
JS-banana <sss213018@163.com>
2022-08-11 18:56:54 +0800

    feat: ts支持

---
+++ scripts/build.js
@@ -0,0 +1,10 @@
+// const fg = require('fast-glob');
+// // const config = require('./config');
+
+// const getPackages = async () => {
+//   const entries = await fg('packages/**/*.js', { deep: 2 });
+//   console.log('entries', entries);
+// };
+// async function getPackages() {}
+
+// getPackages();
---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+exports.alias = {
+  packages: path.resolve(__dirname, '../packages'),
+  examples: path.resolve(__dirname, '../examples'),
+  docs: path.resolve(__dirname, '../docs'),
+  '@ah-ailpha/components': path.resolve(__dirname, '../packages/components'),
+};
